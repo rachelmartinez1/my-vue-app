10b8422df88b1dc2d473f36e51754104409d738e
Rachel Martinez <rachelmartinez101@gmail.com>
2020-03-25 21:42:40 -0600

    Adding additional files

---
+++ main.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+    el: '#app', 
+    data: {
+        product: 'Socks',
+        image: './images/palm.JPG',
+        url: 'https://www.youtube.com/watch?v=cN7P_SgWivk'
+    }
+})
